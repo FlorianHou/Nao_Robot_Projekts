a693ca535a9b3827300b14c5efe99c7519c2337d
Florian Hou <florianhou@hotmail.com>
2020-08-18 18:54:50 +0200

    2020_08_18--Dreiecks_Koordination_Korregieren

--- klein/Dreiecks/PnP_Solver/PnP_Solver.py
+++ klein/Dreiecks/PnP_Solver/PnP_Solver.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import glob
 
-with np.load("klein\Dreiecks\PnP_Solver\datei\zusammen_oben_320.npz") as file:
+with np.load(".\klein\\Dreiecks\\PnP_Solver\\datei\\zusammen_oben_320.npz") as file:
     mtx, dist, _, _ = [file[i] for i in ("mtx", "dist", "rvecs", "tvecs")]
 
 
@@ -23,8 +23,8 @@ def ecks_dreieck():
 
 def PnP_Solve():
 
-    objp = np.array([[0, 0, 0], [-7.49, 11.2, 0], [7.45, 11.2, 0], [0, 10.3, 0],
-                    [-2.8, 4.7, 0], [2.8, 4.7, 0]], np.float32) # Pinkt in realem Welt
+    objp = np.array([[0, 0, 0], [0, -7.49, 11.2], [0, 7.45, 11.2], [0, 0, 10.3],
+                    [0,-2.8, 4.7], [0, 2.8, 4.7]], np.float32) # Pinkt in realem Welt
     corners2 = ecks_dreieck() # Aus Bild
     ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
     rvecs_tr=cv.Rodrigues(rvecs)[0] #RotationsVektor zu Rotationstransform
@@ -33,7 +33,7 @@ def PnP_Solve():
 
 axis = np.float32([[5, 0, 0], [0, 5, 0], [0, 0, -5]]).reshape(-1, 3)
 
-img = cv.imread("klein\Dreiecks\PnP_Solver\datei\\322.png")
+img = cv.imread("klein\\Dreiecks\\PnP_Solver\\datei\\322.png")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
