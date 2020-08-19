ea12e568f03bafc5241457a446e641c38623acad
Zhaoyang Hou <florianhou@hotmail.com>
2020-08-19 23:08:29 +0200

    Get_Image sauber

--- Calibieren/get_image.py
+++ Calibieren/get_image.py
@@ -9,56 +9,55 @@ import sys
 
 # 0 ist obere Kamera
 CamId = 0
-# 3 ist k4VGA,2 ist VGA
+# 3 ist k4VGA(1280*960),2 ist VGA(VGA funktionert nicht, andere sind Ok), 1 ist 320*240
 Res = 1
-#BGR-13, YUV422-9
+#BGR-13
 ColorSpace = 13
 # FPS
 fps = 30
-# tempFile
-# frame_path = tempfile.mkstemp()[1]
+
 try:
-    app = qi.Application(url="tcp://10.0.158.231:9559")
+    app = qi.Application(url="tcp://10.0.158.231:9559")  # IP der NAO
 except RuntimeError:
-    print"error!!"
+    print "error!!"
     sys.exit(1)
 
 app.start()
 session = app.session
 # Services
-motion = session.service("ALMotion")
-pose = session.service("ALRobotPosture")
 video_cam = session.service("ALVideoDevice")
 # Subscribe Kamera
+# Komischerweise kann ein gleicher Name des Subscriber(hier ist "Kamera_Get") nicht mehr als 6 mals angewendet werden, wenn Ein Fehler gibt,
+# Versuchen Sie zuerst diesen Name zu anderen.
 nameId = video_cam.subscribeCamera("Kamera_Get", CamId, Res, ColorSpace, fps)
-video_cam.setParameter(0,40,0)
+#File name
 count = 320
 while True:
     # Get Image
     time.sleep(3)
-    video_cam.setParameter(0,43,30)
+    video_cam.setParameter(0, 43, 30)
     image_raw = video_cam.getImageRemote(nameId)
     image_array_binary = image_raw[6]
     w = image_raw[0]
     h = image_raw[1]
+
     image_array_string = str(bytearray(image_array_binary))
     image_array = np.fromstring(image_array_string, np.uint8)
     image_array = image_array.reshape(h, w, 3)
-    # print len(image_array)
     image_array_bgr = image_array
-    # #show Image
+    # #Show Bild
     cv.imshow("img", image_array_bgr)
     k = cv.waitKey(0)
+    # k = cv.waitKey(1) # Kontinuierliche Frame
     cv.destroyAllWindows()
-    # cv.imwrite("lol.png",image_array_bgr)
-    # Save, Destory, Next, Destory
+    # druck "s" zu speichen, "n" zu naeschste Bild, q zu schliessen
     if k == ord("s"):
         count += 1
         name = str(count).zfill(3) + ".png"
-        cv.imwrite("./Calibieren/datei/foto_1/"+ name, image_array_bgr)
-    elif k == ord("n"):
+        cv.imwrite("./Calibieren/datei/foto_1/" + name, image_array_bgr)
+    if k == ord("n"):
         pass
-    else:
+    if k == ord("q"):
         video_cam.unsubscribe(nameId)
         break
 # unsubscribe
