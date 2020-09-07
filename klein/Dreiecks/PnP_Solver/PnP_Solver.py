7ea5184c80f96158ce0ef323165dc4eb7d1de6fa
Florian Hou <florianhou@hotmail.com>
2020-09-07 10:33:26 +0200

    2020_09_20_Ip Korrigieren und Länge der Kante der DreiecksKorrigieren

--- klein/Dreiecks/PnP_Solver/PnP_Solver.py
+++ klein/Dreiecks/PnP_Solver/PnP_Solver.py
@@ -6,7 +6,7 @@ import math
 import almath
 import time
 
-url = "tcp://192.168.1.101:11148"
+url = "tcp://192.168.1.101:12916"
 session = qi.Session()
 session.connect(url)
 motion = session.service("ALMotion")
@@ -39,22 +39,23 @@ def PnP_Solve():
     #                 [-2.8, 4.7,0], [2.8, 4.7,0]], np.float32) # Punkt in realem Welt
     # objp = np.array([[0,0,0], [-7.5, 11.2,0],[7.5,11.2,0],
     #                     [0,10.3,0], [-2.9, 4.7,0],[2.9, 4.7,0]], np.float32)
-    objp = np.array([[0, 0, 0], [-2.9, 4.7, 0], [2.9, 4.7, 0], 
-                        [0, 10.3, 0], [-7.5, 11.2, 0], [7.5, 11.2, 0]], np.float32)
+    objp = np.array([[0, 0, 0], [-1.85, 4.65, 0], [1.85, 4.65, 0], 
+                        [0, 10.3, 0], [-7.5, 11.25, 0], [7.5, 11.25, 0]], np.float32)/100
     corners2 = ecks_dreieck()  # Aus Bild
 
     b,g,r = cv.split(img)
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     corner_2 = cv.cornerSubPix(g, corners2,(5,5),(-1,-1),criteria)
     
-    ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist, flags=cv.SOLVEPNP_IPPE)
+    ret, rvecs, tvecs = cv.solvePnP(objp, corner_2, mtx, dist,flags=cv.SOLVEPNP_IPPE)
+    # ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
     rvecs_tr = cv.Rodrigues(rvecs)[0]  # RotationsVektor zu Rotationstransform
     return rvecs, rvecs_tr, tvecs, corner_2
 
 
 def Cam2Ziel_OP():
     a = np.array(PnP_Ergebnis_dict["rvecs_tr"])
-    b = np.array(PnP_Ergebnis_dict["tvecs"])/100 # Einheit von cm zu m
+    b = np.array(PnP_Ergebnis_dict["tvecs"]) # Einheit von cm zu m
     Transform_Cam2Ziel_OP = np.hstack((a, b))
     Transform_Cam2Ziel_OP = almath.Transform(Transform_Cam2Ziel_OP.flatten())
     return Transform_Cam2Ziel_OP
@@ -66,8 +67,8 @@ def Robot2Ziel_ROB(robot2CamROB):
     RotX_1 = almath.Transform.fromRotX(-math.pi/2)
     RotY_2 = almath.Transform.fromRotY(-math.pi/2)
     RotX_2 = almath.Transform.fromRotX(math.pi/2)
-    Tr_X = almath.Transform.fromPosition(0., 0.5, 0.)
-    robot2CamOP = robot2CamROB * RotZ_1 * RotX_1
+    Tr_X = almath.Transform.fromPosition(0.0, 0., 0.)
+    robot2CamOP = robot2CamROB * RotZ_1 * RotX_1 
     robot2ZielOP = robot2CamOP * Cam2Ziel_OP()
     robot2ZielROB = robot2ZielOP * RotY_2 * RotX_2 * Tr_X
     print robot2ZielROB
@@ -81,9 +82,9 @@ def get_ROB2CAM():
     return robot2CamROB
 
 
-axis = np.float32([[15, 0, 0], [0, 15, 0], [0, 0, -15]]).reshape(-1, 3)
+axis = np.float32([[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]]).reshape(-1, 3)
 
-img = cv.imread("klein/Dreiecks/PnP_Solver/datei/962.png")
+img = cv.imread("klein/Dreiecks/PnP_Solver/datei/961.png")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 rvecs, rvecs_tr, tvecs, corners2 = PnP_Solve()
@@ -101,7 +102,7 @@ robot2CamROB = get_ROB2CAM()
 robot2ZielROB = Robot2Ziel_ROB(robot2CamROB)
 robot2ZielROB_Pose2D = almath.pose2DFromTransform(robot2ZielROB).toVector()
 img = draw(img, corners2, imgpts)
-cv.imshow("img", img)
+cv.imshow("img", cv.resize(img,(640,480)))
 k = cv.waitKey(0)
 if k == ord("s"):
     cv.imwrite("axis", img)
@@ -109,8 +110,12 @@ cv.destroyAllWindows()
 print robot2ZielROB_Pose2D
 print 180*(robot2ZielROB_Pose2D[2]/math.pi)
 print almath.position6DFromTransform(robot2ZielROB)
-# visionCompass.moveTo(robot2ZielROB_Pose2D[0]/100,robot2ZielROB_Pose2D[1]/100,robot2ZielROB_Pose2D[2])
+result = visionCompass.setResolution(1)
+print result
 
-time.sleep(3)
-motion.setAngles(["HeadPitch"], [15*(math.pi/180)], 0.1)
-motion.setAngles(["HeadYaw"], [0.], 0.1)
+# visionCompass.moveTo(robot2ZielROB_Pose2D[0],robot2ZielROB_Pose2D[1],robot2ZielROB_Pose2D[2])
+motion.moveTo(robot2ZielROB_Pose2D[0],robot2ZielROB_Pose2D[1],robot2ZielROB_Pose2D[2])
+# motion.moveTo(0,0,math.pi*(180/180))
+time.sleep(1)
+motion.setAngles(["HeadPitch"], [10*(math.pi/180)], 0.1)
+motion.setAngles(["HeadYaw"], [0*(math.pi/180)], 0.1)
