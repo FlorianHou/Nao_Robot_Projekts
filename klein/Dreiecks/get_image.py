2231c57cabe4c3f8b226eefb6594a980b3f38a06
Florian Hou <florianhou@hotmail.com>
2020-09-07 11:11:14 +0200

    2020_09_07  Parameters,zeigen resize

--- klein/Dreiecks/get_image.py
+++ klein/Dreiecks/get_image.py
@@ -12,7 +12,7 @@ import math
 CamId = 0
 # 3 ist k4VGA,2 ist VGA    plt.imshow(contours,"gray")
 
-Res = 3
+Res = 4
 #BGR-13, YUV422-9
 ColorSpace = 13
 # FPS
@@ -20,8 +20,8 @@ fps = 30
 # tempFile
 # frame_path = tempfile.mkstemp()[1]
 try:
-    # app = qi.Application(url="tcp://10.0.147.226:9559")
-    app = qi.Application(url="tcp://192.168.1.101:11148")
+    # app = qi.Application(url="tcp://10.0.158.231:9559")
+    app = qi.Application(url="tcp://192.168.1.101:4073")
 except RuntimeError:
     print"error!!"
     sys.exit(1)
@@ -34,17 +34,18 @@ pose = session.service("ALRobotPosture")
 video_cam = session.service("ALVideoDevice")
 # Subscribe Kamera
 nameId = video_cam.subscribeCamera("Kamera_Get20", CamId, Res, ColorSpace, fps)
-video_cam.setParameter(0,40,1)
-# video_cam.setParameter(0,43,50)
-video_cam.setParameter(0,11,1)
-# video_cam.setParameter(0,17,700)
-video_cam.setParameter(0,24,1)
+video_cam.setParameter(0,40,0)
+video_cam.setParameter(0,43,80)
+video_cam.setParameter(0,11,0)
+video_cam.setParameter(0,17,700)
+video_cam.setParameter(0,24,2)
 
-count = 960
+count = 2000
 while True:
     # Get Image
     # time.sleep(3)
     # video_cam.setParameter(0,43,30)
+    time.sleep(2)
     image_raw = video_cam.getImageRemote(nameId)
     image_array_binary = image_raw[6]
     w = image_raw[0]
@@ -55,7 +56,7 @@ while True:
     # print len(image_array)
     image_array_bgr = image_array
     # #show Image
-    cv.imshow("img", image_array_bgr)
+    cv.imshow("img", cv.resize(image_array_bgr, (640,480)))
     k = cv.waitKey(0)
     cv.destroyAllWindows()
     # cv.imwrite("lol.png",image_array_bgr)
@@ -65,7 +66,8 @@ while True:
         name = str(count).zfill(3) + ".png"
         cv.imwrite("./klein/Dreiecks/datei/"+ name, image_array_bgr)
         cv.imwrite("./klein/Dreiecks/PnP_Solver/datei/"+ name, image_array_bgr)
-
+        video_cam.unsubscribe(nameId)
+        break
     elif k == ord("n"):
         pass
     else:
