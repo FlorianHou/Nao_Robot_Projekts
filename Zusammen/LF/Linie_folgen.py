44f8d4c8945483358ef40601f47349ad1be8242a
Florian Hou <florianhou@hotmail.com>
2020-09-07 10:43:52 +0200

    2020_09_20_Sauber,ColorSpaceToYuv

--- Zusammen/LF/Linie_folgen.py
+++ Zusammen/LF/Linie_folgen.py
@@ -11,8 +11,8 @@ import time
 CamId = 1
 # 3 ist k4VGA,2 ist VGA,1 kQVGA
 Res = 1
-#BGR-13, YUV422-9
-ColorSpace = 13
+#BGR-13, YUV422-9, Yuv-1
+ColorSpace = 1
 # FPS
 fps = 30.0
 
@@ -23,13 +23,11 @@ def Kamera_Vorbrereiten():
                                                    ColorSpace, fps)
     time.sleep(3)
     motion_service.rest()
-    posture_service.goToPosture("Stand", 0.3)
+    posture_service.goToPosture("Stand", 0.6)
     motion_service.setIdlePostureEnabled("Body", False)
-    motion_service.moveInit()
     motion_service.setStiffnesses("Head", 1.0)
     motion_service.setAngles(["Head"], (0., (35 / 180) * math.pi), 0.02)
-    effector = 43  # Foucs
-    video_cam_service.setParameter(CamId, effector, 30)
+    video_cam_service.setParameter(CamId, 43, 30)
     video_cam_service.setParameter(CamId, 0, 50)
     video_cam_service.setParameter(CamId, 1, 32)
     video_cam_service.setParameter(CamId, 11, 0)
@@ -51,19 +49,14 @@ def image_array(raw_datei):
     image_array_bgr = image_array.reshape(h, w, 3)
     return image_array_bgr
 
-
-def image_array_2(raw_datei):
+def image_array_Y(raw_datei):
     """Raw_Datei zu BGR_Array"""
     image_array_binary = raw_datei[6]
     w = raw_datei[0]
     h = raw_datei[1]
-    image_array_bin = bytearray(image_array_binary)
-    image_array_0 = np.frombuffer(image_array_bin, dtype=np.uint8)
-    image_array_1 = np.frombuffer(image_array_binary, dtype=np.uint8)
-    image_array_2 = np.fromfile(image_array_binary, dtype=np.uint8)
-    image_array_bgr = image_array.reshape(h, w, 3)
-    return image_array_bgr
-
+    image_array_string = str(bytearray(image_array_binary))
+    image_array = np.fromstring(image_array_string, np.uint8)
+    return image_array.reshape(h, w)
 
 def close_Camera():
     return video_cam_service.unsubscribe(nameId_Cam)
@@ -76,7 +69,6 @@ def MittelPunkt_folgen(image):
     image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     image_gray = cv.GaussianBlur(image_gray, (5, 5), 0)
     ret, thresh = cv.threshold(image_gray, 80, 255, cv.THRESH_BINARY)
-
     # thresh = cv.adaptiveThreshold(image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 17, 2)
     signed_thresh = thresh[start_height].astype(np.int16)
     diff = np.diff(signed_thresh)
@@ -92,7 +84,37 @@ def MittelPunkt_folgen(image):
         cv.circle(image_rgb, (middle, start_height), 15, (0, 0, 255), -1)
         exzentierung = middle - w / 2
         print exzentierung, middle, w / 2
-        Move(exzentierung)
+        # Move(exzentierung)
+        # else:
+    #     start_height -= 5
+    #     start_height = start_height % h
+    #     no_points_count += 1
+    #     if Count > 20:
+    #         StopAll()
+    return middle, image_rgb, thresh
+
+
+def MittelPunkt_folgen_OnlyY(img_Y):
+    h, w = img_Y.shape[:2]
+    start_height = h - 10
+    img_Y = cv.GaussianBlur(img_Y, (5, 5), 0)
+    ret, thresh = cv.threshold(img_Y, 80, 255, cv.THRESH_BINARY)
+    # thresh = cv.adaptiveThreshold(image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 17, 2)
+    signed_thresh = thresh[start_height].astype(np.int16)
+    diff = np.diff(signed_thresh)
+    points = np.where(np.logical_or(diff > 220, diff < -220))
+    cv.line(image_rgb, (0, start_height), (w, start_height), (0, 255, 0), 3)
+
+    if len(points) > 0 and len(points[0]) > 1:  # schwarze linie gefunden
+        middle = (points[0][0] + points[0][-1]) / 2
+        cv.circle(image_rgb, (points[0][0], start_height), 15, (255, 0, 0),
+                  -1)  # zeichnen rechte grenze
+        cv.circle(image_rgb, (points[0][-1], start_height), 15, (0, 255, 0),
+                  -1)
+        cv.circle(image_rgb, (middle, start_height), 15, (0, 0, 255), -1)
+        exzentierung = middle - w / 2
+        print exzentierung, middle, w / 2
+        # Move(exzentierung)
         # else:
     #     start_height -= 5
     #     start_height = start_height % h
@@ -139,8 +161,8 @@ if __name__ == "__main__":
         firsttime = True
         while True:
             raw = get_raw()
-            image_array_bgr = image_array(raw)
-            middle, image_rgb, thresh = MittelPunkt_folgen(image_array_bgr)
+            image_array = image_array(raw)
+            middle, image_rgb, thresh = MittelPunkt_folgen(image_array)
             cv.imshow("Linie_Folgen", image_rgb)
             k = cv.waitKey(1)
             if k == ord("q"):
