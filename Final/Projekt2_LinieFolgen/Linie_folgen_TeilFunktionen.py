d75ca129e9381eeeb2570e177867593ac224b385
Florian Hou <florianhou@hotmail.com>
2020-09-10 11:04:16 +0200

    2020_09_10 ordnen und datei zu Final verschieben

---
+++ Final/Projekt2_LinieFolgen/Linie_folgen_TeilFunktionen.py
@@ -0,0 +1,176 @@
+import qi
+import numpy as np
+import almath
+import cv2 as cv
+from matplotlib import pyplot as plt
+import math
+from simple_pid import PID
+import time
+
+# 0 ist obere Kamera
+CamId = 1
+# 3 ist k4VGA,2 ist VGA,1 kQVGA
+Res = 1
+#BGR-13, YUV422-9, Yuv-1
+ColorSpace = 1
+# FPS
+fps = 30.0
+
+
+def Kamera_Vorbrereiten():
+    video_cam_service = session.service("ALVideoDevice")
+    nameId_Cam = video_cam_service.subscribeCamera("Kamera_Get16", CamId, Res,
+                                                   ColorSpace, fps)
+    time.sleep(3)
+    motion_service.rest()
+    posture_service.goToPosture("Stand", 0.6)
+    motion_service.setIdlePostureEnabled("Body", False)
+    motion_service.setStiffnesses("Head", 1.0)
+    motion_service.setAngles(["Head"], (0., (35 / 180) * math.pi), 0.02)
+    video_cam_service.setParameter(CamId, 43, 30)
+    video_cam_service.setParameter(CamId, 0, 50)
+    video_cam_service.setParameter(CamId, 1, 32)
+    video_cam_service.setParameter(CamId, 11, 0)
+    video_cam_service.setParameter(CamId, 17, 16)
+    return video_cam_service, nameId_Cam
+
+
+def get_raw():
+    return video_cam_service.getImageRemote(nameId_Cam)
+
+
+def image_array(raw_datei):
+    """Raw_Datei zu BGR_Array"""
+    image_array_binary = raw_datei[6]
+    w = raw_datei[0]
+    h = raw_datei[1]
+    image_array_string = str(bytearray(image_array_binary))
+    image_array = np.fromstring(image_array_string, np.uint8)
+    image_array_bgr = image_array.reshape(h, w, 3)
+    return image_array_bgr
+
+def image_array_Y(raw_datei):
+    """Raw_Datei zu BGR_Array"""
+    image_array_binary = raw_datei[6]
+    w = raw_datei[0]
+    h = raw_datei[1]
+    image_array_string = str(bytearray(image_array_binary))
+    image_array = np.fromstring(image_array_string, np.uint8)
+    return image_array.reshape(h, w)
+
+def close_Camera():
+    return video_cam_service.unsubscribe(nameId_Cam)
+
+
+def MittelPunkt_folgen(image):
+    h, w = image.shape[:2]
+    start_height = h - 5
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image_gray = cv.GaussianBlur(image_gray, (5, 5), 0)
+    ret, thresh = cv.threshold(image_gray, 80, 255, cv.THRESH_BINARY)
+    # thresh = cv.adaptiveThreshold(image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 17, 2)
+    signed_thresh = thresh[start_height].astype(np.int16)
+    diff = np.diff(signed_thresh)
+    points = np.where(np.logical_or(diff > 220, diff < -220))
+    cv.line(image_rgb, (0, start_height), (w, start_height), (0, 255, 0), 1)
+
+    if len(points) > 0 and len(points[0]) > 1:  # schwarze linie gefunden
+        middle = (points[0][0] + points[0][-1]) / 2
+        cv.circle(image_rgb, (points[0][0], start_height), 15, (255, 0, 0),
+                  -1)  # zeichnen rechte grenze
+        cv.circle(image_rgb, (points[0][-1], start_height), 15, (0, 255, 0),
+                  -1)
+        cv.circle(image_rgb, (middle, start_height), 15, (0, 0, 255), -1)
+        exzentierung = middle - w / 2
+        print exzentierung, middle, w / 2
+        # Move(exzentierung)
+        # else:
+    #     start_height -= 5
+    #     start_height = start_height % h
+    #     no_points_count += 1
+    #     if Count > 20:
+    #         StopAll()
+    return middle, image_rgb, thresh
+
+
+def MittelPunkt_folgen_OnlyY(img_Y):
+    h, w = img_Y.shape[:2]
+    start_height = h - 10
+    img_Y = cv.GaussianBlur(img_Y, (5, 5), 0)
+    ret, thresh = cv.threshold(img_Y, 80, 255, cv.THRESH_BINARY)
+    # thresh = cv.adaptiveThreshold(image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 17, 2)
+    signed_thresh = thresh[start_height].astype(np.int16)
+    diff = np.diff(signed_thresh)
+    points = np.where(np.logical_or(diff > 220, diff < -220))
+    cv.line(image_rgb, (0, start_height), (w, start_height), (0, 255, 0), 3)
+
+    if len(points) > 0 and len(points[0]) > 1:  # schwarze linie gefunden
+        middle = (points[0][0] + points[0][-1]) / 2
+        cv.circle(image_rgb, (points[0][0], start_height), 15, (255, 0, 0),
+                  -1)  # zeichnen rechte grenze
+        cv.circle(image_rgb, (points[0][-1], start_height), 15, (0, 255, 0),
+                  -1)
+        cv.circle(image_rgb, (middle, start_height), 15, (0, 0, 255), -1)
+        exzentierung = middle - w / 2
+        print exzentierung, middle, w / 2
+        # Move(exzentierung)
+        # else:
+    #     start_height -= 5
+    #     start_height = start_height % h
+    #     no_points_count += 1
+    #     if Count > 20:
+    #         StopAll()
+    return middle, image_rgb, thresh
+
+
+def Move(exzentierung):
+    x_Speed = 0.02
+    y_Speed = 0.0
+    kp = 0.01
+    ki = 0.0
+    kd = 50.0
+
+    pid = PID(kp, ki, kd, setpoint=0.)
+    global firsttime
+    if firsttime:
+        pid.output_limits = (-1., 1.)
+        firsttime = False
+
+    rot = (math.pi / 10) * pid(exzentierung)
+    print rot
+    # rot = kp * (math.pi / 4) * (-exzentierung * 2/ w)
+    motion_service.move(x_Speed, y_Speed, rot)
+
+
+def StopAll():
+    motion_service.stopMove()
+    time.sleep(5)
+    motion_service.rest()
+
+
+if __name__ == "__main__":
+    try:
+        app = qi.Application(url="tcp://10.0.158.231:9559")
+        app.start()
+        session = app.session
+        motion_service = session.service("ALMotion")
+        posture_service = session.service("ALRobotPosture")
+        video_cam_service, nameId_Cam = Kamera_Vorbrereiten()
+        time.sleep(5)
+        firsttime = True
+        while True:
+            raw = get_raw()
+            image_array = image_array(raw)
+            middle, image_rgb, thresh = MittelPunkt_folgen(image_array)
+            cv.imshow("Linie_Folgen", image_rgb)
+            k = cv.waitKey(1)
+            if k == ord("q"):
+                cv.destroyAllWindows()
+                StopAll()
+                break
+            # time.sleep(1)
+
+    except RuntimeError:
+        StopAll()
+        app.stop()
---
+++ Final/Projekt2_LinieFolgen/Linie_folgen_Zusammen.py
@@ -0,0 +1,144 @@
+import qi
+import numpy as np
+import cv2 as cv
+import math
+from simple_pid import PID
+import time
+import uuid
+import sys
+
+# 0 ist obere Kamera, 1 ist unter
+CamId = 1
+# 3 ist k4VGA,2 ist VGA,1 kQVGA
+Res = 0
+#BGR-13, YUV422-9, Yuv-1
+ColorSpace = 0
+# FPS
+fps = 60
+
+try:
+    # app = qi.Application(url="tcp://10.0.158.231:9559")
+    app = qi.Application(url="tcp://10.0.147.226:9559")    
+    app.start()
+    session = app.session
+    motion_service = session.service("ALMotion")
+    posture_service = session.service("ALRobotPosture")
+
+    # Kamera_Vorbrereiten
+    video_cam_service = session.service("ALVideoDevice")
+    subName = str(uuid.uuid4())
+    nameId_Cam = video_cam_service.subscribeCamera(subName, CamId, Res,
+                                                   ColorSpace, fps)
+    time.sleep(3)
+    motion_service.rest()
+    posture_service.goToPosture("Stand", 0.6)
+    motion_service.setIdlePostureEnabled("Body", False)
+    motion_service.setStiffnesses("Head", 1.0)
+    motion_service.setAngles(["Head"], (0., (35 / 180) * math.pi), 0.02)
+    video_cam_service.setParameter(CamId, 43, 30)
+    video_cam_service.setParameter(CamId, 0, 50)
+    video_cam_service.setParameter(CamId, 1, 32)
+    video_cam_service.setParameter(CamId, 11, 0)
+    video_cam_service.setParameter(CamId, 17, 80)
+    print "nameId_Cam: ", nameId_Cam
+
+    time.sleep(3)
+    firsttime = True
+    no_points_count = 0
+
+    kp = 1.2
+    ki = 0.
+    kd = 0.5
+    pid = PID(kp, ki, kd, setpoint=0.)
+
+    while True:
+        start = time.time()
+
+        # Get RawImage
+        raw = video_cam_service.getImageRemote(nameId_Cam)
+
+        # Raw_Datei zu BGR_Array
+        image_array_binary = raw[6]
+        w = raw[0]
+        h = raw[1]
+        image_array_string = bytearray(image_array_binary)
+        image_array = np.frombuffer(image_array_string, np.uint8)
+        image_array = image_array.reshape(h, w)
+
+        # MittelPunkt_folgen_OnlyY
+        img_Y = image_array.copy()
+        img_Y = img_Y[:,5:w-5,...]
+
+        h, w = img_Y.shape[:2]
+        image_bgr = cv.cvtColor(img_Y, cv.COLOR_GRAY2BGR)
+        start_height = h - 4
+        img_Y = cv.medianBlur(img_Y, 5)
+        img_Y = cv.GaussianBlur(img_Y, (3,3), 0)
+        ret, thresh = cv.threshold(img_Y, 75, 255, cv.THRESH_BINARY)
+        thresh_bgr = cv.cvtColor(thresh, cv.COLOR_GRAY2BGR)
+        # thresh = cv.adaptiveThreshold(image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 17, 2)
+        signed_thresh = thresh[start_height].astype(np.int16)
+        diff = np.diff(signed_thresh)
+        points = np.where(np.logical_or(diff > 220, diff < -220))
+        cv.line(image_bgr, (0, start_height), (w, start_height), (0, 255, 0), 3)
+
+        if len(points) > 0 and len(points[0]) > 1:  # schwarze linie gefunden
+            middle = (float(points[0][0]) + float(points[0][1])) / 2
+            exzentierung = (middle - w/2)/(w/2) # in Prozent
+
+            cv.circle(image_bgr, (points[0][0], start_height), 5, (255, 0, 0), -1)  # zeichnen rechte grenze
+            cv.circle(image_bgr, (points[0][1], start_height), 5, (255, 0, 0), -1)
+            cv.circle(image_bgr, (int(middle), start_height), 5, (0, 0, 255), -1)
+            cv.line(image_bgr, (int(middle), h/2), (int(middle), h), (0,0,255), 3)
+            font = cv.FONT_HERSHEY_SIMPLEX
+            cv.putText(image_bgr, str(round(float(exzentierung*100),2))+"%",(int(middle), h-20), font, 0.5,(0,0,255),2,cv.LINE_AA)
+
+            print "Exzentierung: ", exzentierung*100, "%", "\n", "middle: ", middle, "\n", "w/2: ", w/2
+
+            # Move(exzentierung)
+            x_Speed = 0.06
+            y_Speed = 0.0
+
+            if firsttime:
+                pid.output_limits = (-2., 2.)
+                firsttime = False
+            
+            u = pid(exzentierung)
+            rot = (math.pi * 20./180.) * u
+            print "Rot: ", 180*(rot/np.pi)
+            print "u: ", u
+
+            #ALMove
+            motion_service.move(x_Speed, y_Speed, rot)
+        else:
+            start_height -= 5
+            start_height = start_height % h
+            no_points_count += 1
+            if no_points_count > 20:
+                # StopAll
+                motion_service.stopMove()
+                time.sleep(5)
+                motion_service.rest()
+                print "Kein Bahn."
+                raise RuntimeError
+        # Zeigen
+        cv.imshow("Linie_Folgen", np.hstack((cv.resize(image_bgr,(640, 480)),cv.resize(thresh_bgr,(640,480)))))
+        k = cv.waitKey(1)
+        if k == ord("q"):
+            cv.destroyAllWindows()
+
+            # StopAll
+            motion_service.stopMove()
+            time.sleep(5)
+            motion_service.rest()
+            break
+        end = time.time()
+        print "Zeit: ", end - start
+
+except RuntimeError:
+    # StopAll
+    motion_service.stopMove()
+    time.sleep(5)
+    motion_service.rest()
+    app.stop()
+    sys.exit()
